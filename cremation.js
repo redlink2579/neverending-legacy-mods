966a3ac43326fbb6c8cd4bc6ea67ffb7c7b105ee
Owen Parker <Oheadthaboss@gmail.com>
2022-03-24 21:55:46 -0400

    Added cremation

---
+++ cremation.js
@@ -0,0 +1,29 @@
+G.AddData({
+	name:'Cremation',
+		//required; the name of the mod - do not change this when updating the mod, as other mods will refer to this mod by its name
+	author:'Owen "Ohead" Parker',
+		//optional; the author of the mod
+	desc:'Adds the cremation option for firekeepers once iron is unlocked',
+		//optional; a simple description of the mod
+	engineVersion:1,
+		//optional; the game engine version that was current when the game was released; the mod won't load if this is older than the current game engine, so mod authors should keep this up to date - alternatively, if this value isn't specified, the mod will work no matter the game engine version (be careful, as other incompatibilities and strange behaviors may arise)
+	requires:[],
+		//optional; a list of mod names that this mod requires; this mod will fail loading if at least one of its required mods fails to load, has errors, or comes after it in the mod list
+		//the specified mod names can be in the form of "mod name*", in which case the game will look for any mod with a name that begins with "mod name"
+	func:function()
+	{
+		G.getUnit('firekeeper').modes['cremate'] = {
+			name:'Cremate corpses',
+			icon:[8,3],
+			desc:'Clear out [corpse]s faster than they can be buried.'
+		};
+		G.getUnit('firekeeper')['effects'].push({
+			type:'convert',
+			from:{'corpse':10,'fire pit':0.01},
+			into:{},
+			every:1,
+			repeat:10,
+			mode:'cremate'
+		});
+	}
+});
