16c157d22755a998164fc2b29e2ae07ff0c9a31b
Owen Parker <Oheadthaboss@gmail.com>
2023-02-13 15:57:07 -0500

    Added GUI to map

--- mods/big-map.js
+++ mods/big-map.js
@@ -5,39 +5,64 @@ G.AddData({
 	engineVersion:1,
 	func:function()
 	{
+		let oldCreateMaps = G.createMaps.bind({});
 		G.createMaps=function()//when creating a new game
 		{
-			G.currentMap=new G.Map(0,Math.floor(prompt('How many times wider shall the world be?') * 24),Math.floor(prompt('How many times taller shall the world be?') * 24));//main world map
+			// Temporary map
+			oldCreateMaps();
 
-			//set starting tile by ranking all land tiles by score and picking one
-			var goodTiles=[];
-			for (var x=1;x<G.currentMap.w-1;x++)
-			{
-				for (var y=1;y<G.currentMap.h-1;y++)
+			function makeMap() {
+				G.dialogue.forceClose();
+
+				widthMultiplier = Math.max(24,Math.floor(widthMultiplier * 24));
+				heightMultiplier = Math.max(24,Math.floor(heightMultiplier * 24));
+				G.currentMap=new G.Map(0,widthMultiplier,heightMultiplier);//main world map
+
+				//set starting tile by ranking all land tiles by score and picking one
+				var goodTiles=[];
+				for (var x=1;x<G.currentMap.w-1;x++)
 				{
-					var land=G.currentMap.tiles[x][y].land;
-					if (!land.ocean) goodTiles.push([x,y,(land.score||0)+Math.random()*2]);
+					for (var y=1;y<G.currentMap.h-1;y++)
+					{
+						var land=G.currentMap.tiles[x][y].land;
+						if (!land.ocean) goodTiles.push([x,y,(land.score||0)+Math.random()*2]);
+					}
 				}
-			}
-			goodTiles.sort(function(a,b){return b[2]-a[2]});
-			var tile=0;
-			if (G.startingType==2) tile=choose(goodTiles);//just drop me wherever
-			else
-			{
-				var ind=0;
-				if (G.startingType==1) ind=Math.floor((0.85+Math.random()*0.15)*goodTiles.length);//15% worst
-				//ind=Math.floor((0.3+Math.random()*0.4)*goodTiles.length);//30% to 70% average
-				else ind=Math.floor((Math.random()*0.15)*goodTiles.length);//15% nicest
-				tile=goodTiles[ind];
-			}
-			tile=G.currentMap.tiles[tile[0]][tile[1]];
-			tile.owner=1;
-			tile.explored=10/100;//create one tile, a tenth of it explored
+				goodTiles.sort(function(a,b){return b[2]-a[2]});
+				var tile=0;
+				if (G.startingType==2) tile=choose(goodTiles);//just drop me wherever
+				else
+				{
+					var ind=0;
+					if (G.startingType==1) ind=Math.floor((0.85+Math.random()*0.15)*goodTiles.length);//15% worst
+					//ind=Math.floor((0.3+Math.random()*0.4)*goodTiles.length);//30% to 70% average
+					else ind=Math.floor((Math.random()*0.15)*goodTiles.length);//15% nicest
+					tile=goodTiles[ind];
+				}
+				tile=G.currentMap.tiles[tile[0]][tile[1]];
+				tile.owner=1;
+				tile.explored=10/100;//create one tile, a tenth of it explored
 
-			G.updateMapForOwners(G.currentMap);
+				G.updateMapForOwners(G.currentMap);
 
-			G.updateMapDisplay();
-			G.centerMap(G.currentMap);
+				G.updateMapDisplay();
+				G.centerMap(G.currentMap);
+				G.setSetting('paused',0);
+			}
+
+			let widthMultiplier = 1;
+			let heightMultiplier = 1;
+			G.setSetting('paused',1);
+			G.dialogue.popup(function(div){
+				var str=
+				`<div class="fancyText title">World Bounds</div><div class="bitBiggerText underTitle">
+				<div class="fancyText par">Width: ${G.field({text:'1',tooltip:'How wide the world should be.',oninput:function(val){widthMultiplier=val}})} times bigger than default</div>
+				<div class="fancyText par">Height: ${G.field({text:'1',tooltip:'How tall the world should be.',oninput:function(val){heightMultiplier=val}})} times bigger than default</div>
+				${G.button({text:'Generate',onclick:makeMap})}`;
+				return str;
+			},'wideDialogue')
+			
+			
 		}
 	}
 });
