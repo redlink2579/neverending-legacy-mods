0eff038c81a86affa9bab4dec596cedbacbe9b6b
Owen Parker <67942932+TheRealOhead@users.noreply.github.com>
2023-02-14 01:02:35 -0500

    combined the farm types

--- mods/agriculture.js
+++ mods/agriculture.js
@@ -13,7 +13,7 @@ G.AddData({
             desc:'@unlocks [orchard]s, which provide [fruit] much faster than a gather can.<>@unlocks [grain farm]s, which produce [grain]',
             icon:[0,1,'agriculture',23,1],
             cost:{'insight':15},
-            req:{'sedentism':true,'smelting':true},
+            req:{'sedentism':true},
             effects:[
                 {type:'show context',what:['orchard','grain farm']},
             ],
@@ -30,19 +30,25 @@ G.AddData({
                 {type:'gather',context:'grow',what:{'fruit':15}},
                 {type:'mult',value:1.2,req:{'harvest rituals':'on'}}
             ],
-            req:{'agriculture':true},
+            req:{'agriculture':true,'tool-making':true},
             category:'production',
         });
 
         new G.Unit({
-            name:'grain farm',
-            desc:'@generates [grain]<>[grain,Grain] can be baked into [bread], a stable food source.',
+            name:'farm',
+            desc:'@generates various plant resources<>[grain,Grain] can be baked into [bread], a stable food source.<>[cotton,Cotton] is a great material for making cotton',
             icon:[0,1,'agriculture'],
             cost:{},
-            use:{'worker':5,'land':10,'metal tools':5},
+            use:{'land':10},
             upkeep:{'coin':0.5},
+            modes:{
+                'off':G.MODE_OFF,
+                'grain':{name:'Grain farming',icon:[2,1,'agriculture'],desc:'Farm for [grain]',use:{'worker':5,'metal tools':5}},
+                'cotton':{name:'Cotton farming',icon:[3,1,'agriculture'],desc:'Farm for [cotton]',use:{'worker':3,'stone tools':3}},
+            },
             effects:[
-                {type:'gather',context:'grow',what:{'grain':15}},
+                {type:'gather',context:'grow',what:{'grain':15},mode:'grain'},
+                {type:'gather',context:'grow',what:{'cotton':15},mode:'cotton'},
                 {type:'mult',value:1.2,req:{'harvest rituals':'on'}}
             ],
             req:{'agriculture':true},
@@ -53,6 +59,14 @@ G.AddData({
             name:'grain',
             desc:'[grain,Grain] can be ground into flour and baked into [bread].',
             icon:[2,1,'agriculture'],
+            turnToByContext:{'decay':{'grain':0.2,'spoiled food':0.8}},
+            category:'misc',
+        });
+
+        new G.Res({
+            name:'cotton',
+            desc:'[cotton,Cotton] can be sewn into [basic clothes,clothes].',
+            icon:[3,1,'agriculture'],
             turnToByContext:{'decay':{'grain':0.2,'spoiled food':0.8}},
             category:'misc',
         });
